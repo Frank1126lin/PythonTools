5dba44e643406322cae0af4813ed0b7369c18771
Frank1126lin <37039306+Frank1126lin@users.noreply.github.com>
2019-08-30 11:15:11 +0800

    update
    更新相关内容

--- re_practice.py
+++ re_practice.py
@@ -4,6 +4,8 @@
 # @Author: Frank1126lin
 # @Date  : 2019/8/29
 
+
+import re
 '''正则速记'''
 """
 正则表达式
@@ -13,8 +15,8 @@ A. 原子：组成正则的基本单位就是原子
     2. 所有不可见字符也都是原子: \n \t \r
     3. 表示所有数字的原子：\d [表示0-9任意一个字符]
     4. 表示所有非数字的原子： \D 【表示除了0-9任意一个字符】
-    5. 表示能够当作变量使用的原子： \w 【表示0-9a-zA-Z任意一个字符】
-    6. 表示不能够当作变量使用的原子： \W 【表示除了0-9a-zA-Z任意一个字符】
+    5. 表示能够当作变量使用的原子： \w 【表示0-9a-zA-Z_任意一个字符】
+    6. 表示不能够当作变量使用的原子： \W 【表示除了0-9a-zA-Z_任意一个字符】
     7. 表示不可见字符都是原子： \s 【表示任意一个不可见字符】
     8. 表示可见字符都是原子： \S 【表示任意一个可见字符】
     
@@ -30,16 +32,85 @@ C. 元字符： 用于修饰原子的符号
     8. {m} :指定原子数量必须m个
     9. ^ :限制内容必须在整个字符串的开头位置
     10. $ :限制内容必须在整个字符串的结束位置
-    11. . :表示任意一个字符 除了\n
+    11. . :表示任意一个字符 除了换行符\n
     12. | :唯一一个逻辑运算符或（万能钥匙）
-    13. \b 词边界：能够当作英文单词的分隔符号,即除了数字和字母
-    14. \B 非词边界：不能够当作英文单词的分隔符号
+    13. ? :表示非贪婪模式匹配（正则默认是贪婪模式匹配）
+    14. \ :转义字符，用于转义字符串
     
     
 """
+# 1.[]: 原子列表：自定义原子范围 【表示列表中任意一个字符】 [0123456789] =\d
+# txt = '135750+++'
+# result = re.findall('[01357]', txt)
+# print(result)
+# 2. [^] :排除列表：自定义原子范围 【表示列表外任意一个字符】 [^0123456789]
+# txt = '13575024680+++'
+# result = re.findall('[^01357]', txt)
+# print(result)
+# 3. + :1个或以上的指定原子
+# txt = '135750000246800+++'
+# result = re.findall('0+', txt)
+# print(result)
+# * :0个或以上的指定原子
+# txt = '10000200+++'
+# result = re.findall('0*', txt)
+# print(result)
+# 5. ? :0个或1个指定原子
+# txt = '10000200+++'
+# result = re.findall('0?', txt)
+# print(result)
+# 6. {m,n} :指定原子数量最少m个，最多n个
+# txt = '10000200+++'
+# result = re.findall('0{2,5}', txt)
+# print(result)
+# 7. {m,}或{,n} :指定原子数量最少m个或最多n个
+# txt = '10000200+++'
+# result = re.findall('0{,3}', txt)
+# print(result)
+# 8. {m} :指定原子数量必须m个
+# txt = '10000200+++'
+# result = re.search('0{2}', txt)
+# print(result.group())
+# 9. ^ :限制内容必须在整个字符串的开头位置
+# txt = '0000200+++'
+# result = re.findall('^0{2,5}', txt)
+# print(result)
+# 10. $ :限制内容必须在整个字符串的结束位置
+# txt = '1000000'
+# result = re.findall('0{2,5}$', txt)
+# print(result)
+# 11. . :表示任意一个字符 除了换行符\n
+# txt = '10000200+++'
+# result = re.findall('.+', txt)
+# print(result)
+# 12. | :唯一一个逻辑运算符或（万能钥匙）
+# txt = '200'
+# result = re.match('^0$|^[1-9]\d?$|^100$', txt)
+# if result:
+#     print(result.group())
+# else:
+#     print("NONE")
+# 13. ? :表示非贪婪模式匹配（正则默认是贪婪模式匹配）
 
-import re
-str = "www.google.com"
-pattern = re.compile(r'\bgoogle\b')
-result = re.findall(pattern=pattern,string=str)
-print(result)
+# str = "<h1>title</h1>"
+# pattern = re.compile('<.+?>')
+# result = re.findall(pattern=pattern,string=str)
+# print(result)
+
+# 14. \ :转义字符，用于转义字符串
+# txt = 'Apple Mac is $3999'
+# result = re.search('\$\d+', txt)
+# print(result.group())
+
+'''
+函数
+'''
+# 1. complie函数：
+txt = 'The num is 30.04'
+txt_comp = re.compile(r'''
+    \d+  # 小数点前面的数字
+    \.?  # 小数点本身
+    \d*  # 小数点后面的数字
+    ''', re.VERBOSE)
+result = re.search(txt_comp, txt)
+print(result.group())

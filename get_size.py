fbb5a25abade71796ff40cee124e78e9fbcaa422
Frank <37039306+Frank1126lin@users.noreply.github.com>
2020-02-22 09:37:31 +0800

    Update get_size.py
    add size_handle and path validation

--- get_size.py
+++ get_size.py
@@ -7,34 +7,42 @@
 import os
 
 
+def get_size(path):
+    if os.path.isfile(path):
+        return get_file_size(path)
+    if os.path.isdir(path):
+        return get_dir_size(path)
+
+
 def get_file_size(filepath):
-    if not os.path.isfile(filepath):
-        return {'Msg':'不是文件，请检查！'}
-    return os.path.getsize(filepath)
+    size = os.path.getsize(filepath)
+    return {'file path':filepath, 'file size':size_handle(size)}
 
 def get_dir_size(dirpath):
     size = 0
     for root, dirs, files in os.walk(dirpath):
         for f in files:
             size += os.path.getsize(os.path.join(root, f))
-    print(size)
+    return {'dir path': dirpath, 'dir size': size_handle(size)}
+
+
+def size_handle(size):
     if 0 <= size < 1024:
-        return {'size': [size, 'B']}
+        return [size, 'B']
     if 1024 <= size < 1024 ** 2:
         ksize = round(size / 1024, 2)
-        return {'size': [ksize, 'KB']}
+        return [ksize, 'KB']
     if 1024 ** 2 <= size < 1024 ** 3:
         msize = round(size / 1024 ** 2, 2)
-        return {'size': [msize, 'MB']}
+        return [msize, 'MB']
     if 1024 ** 3 <= size < 1024 ** 4:
         gsize = round(size / 1024 ** 3, 2)
-        return {'size': [gsize, 'GB']}
+        return [gsize, 'GB']
     if 1024 ** 4 <= size < 1024 ** 5:
         tsize = round(size / 1024 ** 4, 2)
-        return {'size': [tsize, 'TB']}
+        return [tsize, 'TB']
     if 1024 ** 5 <= size < 1024 ** 6:
         psize = round(size / 1024 ** 4, 2)
-        return {'size': [psize, 'PB']}
+        return [psize, 'PB']
 
-
-print(get_dir_size(os.getcwd()))
+print(get_size(os.getcwd()))

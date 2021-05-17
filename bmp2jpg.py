6e47502590d54c93fc6fa84a6b271b671041e8f8
FRANK <Frank1126.lin@gmail.com>
2021-05-17 12:21:42 +0800

    Update bmp2jpg.py

--- bmp2jpg.py
+++ bmp2jpg.py
@@ -8,25 +8,31 @@ import os
 from PIL import Image
 
 
-def bmp2jpg(path, new_path):
-    if not os.path.isdir(path):
-        print("@请检查是否为文件夹目录！")
-        return
-    for f in os.listdir(path):
-        if not f.endswith(".bmp"):
-            continue
-        new_name = f.replace(".bmp", ".jpg")
-        new_addr = os.path.join(new_path, new_name)
-        img = Image.open(os.path.join(path,f))
-        img.save(new_addr)
-        print(f"@文件已保存至{new_addr}")
+def bmp2jpg(f):
+    # 输入文件全路径f, 保存jpg至当前路径
+    if f.endswith(".bmp"):
+        basename = os.path.basename(f)
+        dirname = os.path.dirname(f)
+        newdir = dirname + "jpg"
+        if not os.path.exists(newdir):
+            os.mkdir(newdir)
+        jpg_name = os.path.join(newdir, basename.replace(".bmp", ".jpg"))
+        # png_name = os.path.join(newdir, basename.replace(".bmp", ".png"))
+        img = Image.open(f)
+        img.save(jpg_name, quality = 95) # quality范围1-95，默认75，默认效果较差。
+        # img.save(png_name)
+        print(f"@文件已保存至{jpg_name}\n")
 
 
+
+def main(path):
+    for i in os.listdir(path):
+        new_path = os.path.join(path, i)
+        if os.path.isdir(new_path):
+            main(new_path)
+        elif os.path.isfile(new_path):
+            print(new_path)
+            bmp2jpg(new_path)
 
 if __name__ == '__main__':
-    path = "tmp"
-    new_path = os.path.join(path, "jpg")
-    print(new_path)
-    if not os.path.exists(new_path):
-        os.mkdir(new_path)
-    bmp2jpg(path, new_path)
+    main(".")
